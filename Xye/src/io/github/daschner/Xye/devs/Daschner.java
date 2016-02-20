573f4a91a0cad3e45c570651c3b09135e5955c2b
Mr. Daschner <Daschner02@hotmail.com>
2016-02-20 00:56:55 -0500

    Added testing initiliazation to Daschner class.

--- Xye/src/io/github/daschner/Xye/devs/Daschner.java
+++ Xye/src/io/github/daschner/Xye/devs/Daschner.java
@@ -4,23 +4,30 @@ public class Daschner
 {
 	public static void preInitialization()
 	{
-		System.out.println("Daschner's preinitialization has started.");
+		System.out.println("Mr. Daschner's preinitialization has started.");
 		
-		System.out.println("Daschner's preinitialization has completed.");
+		System.out.println("Mr. Daschner's preinitialization has completed.");
 	}
 	
 	public static void Initialization()
 	{
-		System.out.println("Daschner's initialization has started.");
-		
-		System.out.println("Daschner's initialization has completed.");
+		System.out.println("Mr. Daschner's initialization has started.");
+		testInitialization();
+		System.out.println("Mr. Daschner's initialization has completed.");
 	}
 	
 	public static void postInitialization()
 	{
-		System.out.println("Daschner's postinitialization has started.");
+		System.out.println("Mr. Daschner's postinitialization has started.");
 		
-		System.out.println("Daschner's postinitialization has completed.");
+		System.out.println("Mr. Daschner's postinitialization has completed.");
+	}
+	
+	public static void testInitialization()
+	{
+		System.out.println("Mr. Daschner's initialization testing has started.");
+		
+		System.out.println("Mr. Daschner's initialization testing has completed.");
 	}
 
 }
