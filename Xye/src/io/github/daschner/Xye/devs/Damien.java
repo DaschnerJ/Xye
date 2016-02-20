a748ca73a32d88aea248040bf8dc4e1b9e61b568
Mr. Daschner <Daschner02@hotmail.com>
2016-02-19 22:27:51 -0500

    Fix.

--- Xye/src/io/github/daschner/Xye/devs/Damien.java
+++ Xye/src/io/github/daschner/Xye/devs/Damien.java
@@ -2,18 +2,29 @@ package io.github.daschner.Xye.devs;
 
 public class Damien 
 {
+	
+	public static int x;
+	
+	//Libraries and APIs
 	public static void preInitialization()
 	{
+		 
+		Damien.x = 0;
 		
 	}
 	
+	
 	public static void Initialization()
 	{
+		
+		System.out.print(x);
 		
 	}
 	
 	public static void postInitialization()
 	{
+		
+		
 		
 	}
 
