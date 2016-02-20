239549d681e461c868388bd86722e9c930ad3285
Mr. Daschner <Daschner02@hotmail.com>
2016-02-19 21:54:40 -0500

    Fix.

--- Xye/src/io/github/daschner/Xye/devs/Damien.java
+++ Xye/src/io/github/daschner/Xye/devs/Damien.java
@@ -12,7 +12,6 @@ public class Damien
 		
 	}
 	
-	
 	public static void postInitialization()
 	{
 		
--- Xye/src/io/github/daschner/Xye/devs/Daschner.java
+++ Xye/src/io/github/daschner/Xye/devs/Daschner.java
@@ -12,7 +12,6 @@ public class Daschner
 		
 	}
 	
-	
 	public static void postInitialization()
 	{
 		
