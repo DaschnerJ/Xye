a22210bc403505e681c78e4719db4cbf2a584683
Mr. Daschner <Daschner02@hotmail.com>
2016-02-19 22:07:40 -0500

    Added initialization statements.

--- Xye/src/io/github/daschner/Xye/devs/Daschner.java
+++ Xye/src/io/github/daschner/Xye/devs/Daschner.java
@@ -4,17 +4,23 @@ public class Daschner
 {
 	public static void preInitialization()
 	{
+		System.out.println("Daschner's preinitialization has started.");
 		
+		System.out.println("Daschner's preinitialization has completed.");
 	}
 	
 	public static void Initialization()
 	{
+		System.out.println("Daschner's initialization has started.");
 		
+		System.out.println("Daschner's initialization has completed.");
 	}
 	
 	public static void postInitialization()
 	{
+		System.out.println("Daschner's postinitialization has started.");
 		
+		System.out.println("Daschner's postinitialization has completed.");
 	}
 
 }
