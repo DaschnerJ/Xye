dd7238823ac6eba53b46cbc89073760b2f285527
Damien <Damien@Acer-laptop1.PK5001Z>
2016-02-19 19:07:45 -0800

    Added example for initialization.

--- Xye/src/io/github/daschner/Xye/devs/Damien.java
+++ Xye/src/io/github/daschner/Xye/devs/Damien.java
@@ -2,19 +2,30 @@ package io.github.daschner.Xye.devs;
 
 public class Damien 
 {
+	
+	public static int x;
+	
+	//Libraries and APIs
 	public static void preInitialization()
 	{
 		
+		int x = 0;
+		
 	}
+	
 	
 	public static void Initialization()
 	{
+		
+		System.out.print(x);
 		
 	}
 	
 	
 	public static void postInitialization()
 	{
+		
+		
 		
 	}
 
