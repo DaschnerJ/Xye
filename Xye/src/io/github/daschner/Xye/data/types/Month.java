eb47ced280dfd5842230e900b35c199ee0da5481
Damien <Damien@Acer-laptop1.PK5001Z>
2016-02-20 20:37:06 -0800

    Added a method to test for the amount of days in a month.

--- Xye/src/io/github/daschner/Xye/data/types/Month.java
+++ Xye/src/io/github/daschner/Xye/data/types/Month.java
@@ -17,5 +17,103 @@ public enum Month {
 		this.month = month;
 	}
 	
+	/**
+	 * Gets the amount of days in a given month.
+	 * 
+	 * @param month - The month of which the number of days is gotten from.
+	 * @param year - The year to check on. This is used to calculate leap days in February.
+	 * @return The amount of days in a month.
+	 */
+	
+	public static int getDaysInMonth(Month month, int year) {
+		
+		if(month != null) {
+		
+		if(month == Month.JANUARY) {
+			
+			return 31;
+			
+		}
+		else if(month == Month.FEBRUARY) {
+			
+			if(year % 4 == 0) {
+				
+				return 29;
+				
+			}
+			else
+			{
+				
+				return 28;
+				
+			}
+			
+		}
+		else if(month == Month.MARCH) {
+			
+			return 31;
+			
+		}
+		else if(month == Month.APRIL) {
+			
+			return 30;
+			
+		}
+		else if(month == Month.MAY) {
+			
+			return 31;
+			
+		}
+		else if(month == Month.JUNE) {
+			
+			return 30;
+			
+		}
+		else if(month == Month.JULY) {
+			
+			return 31;
+			
+		}
+		else if(month == Month.AUGUST) {
+			
+			return 31;
+			
+		}
+		else if(month == Month.SEPTEMBER) {
+			
+			return 30;
+			
+		}
+		else if(month == Month.OCTOBER) {
+			
+			return 31;
+			
+		}
+		else if(month == Month.NOVEMBER) {
+			
+			return 30;
+			
+		}
+		else if(month == Month.DECEMBER) {
+			
+			return 31;
+			
+		}
+		else
+		{
+			
+			return 30;
+			
+		}
+		
+		}
+		else
+		{
+			
+			return 30;
+			
+		}
+		
+	}
 
 }
